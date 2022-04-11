68fc0dbea1af64ed7502dafdfab4893608c06f06
ofumimarcel <ofumimarcel@gmail.com>
2022-04-11 16:13:50 -0700

    Task 4 second fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+* main - Print a single line text
+*
+* Return: Always 0
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
