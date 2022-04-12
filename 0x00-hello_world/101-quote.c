243abd6541b1bb1373d2010cca38571bd4b8183e
ofumimarcel <ofumimarcel@gmail.com>
2022-04-11 17:22:04 -0700

    Second advanced fix c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-* main - prints a line without some commands
-* return: Always 1
+* main - prints a line without some commands and a new line,
+*
+* Return: Always 1.
 */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
