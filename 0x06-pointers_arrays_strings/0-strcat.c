81ba5b157a0330ccc6ecc5ff7cfedadd4f201b60
ofumimarcel <ofumimarcel@gmail.com>
2022-04-25 19:57:10 -0700

    Task Zero: 0x06 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* strcat - Concatenates the string pointed to by src
+* _strcat - Concatenates the string pointed to by src
 *          null byte, to the end of the string pointed to by @dest
 * @dest: A pointer to the string to be concatenated upon.
 * @src: The source string to be appended to @dest.
@@ -9,10 +9,20 @@
 */
 char *_strcat(char *dest, const char *src)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+int i;
+int j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
 return (dest);
 }
