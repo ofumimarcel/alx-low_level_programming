6a6b77c49929d45364c9773079829e5866f0a238
Ofushi Orisaremi <ofumimarcel@gmail.com>
2023-08-31 15:45:34 -0700

    Readme created and task 0 of 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @n: unsigned int
+ * Return: binary number
+ */
+ 
+void print_binary(unsigned long int n)
+{
+
+	unsigned long int n_copy = n, msk = 1;
+	int len = 0;
+
+	while (n_copy > 0)
+	{
+		len++;
+		n_copy >>= 1;
+	}
+
+	len -= 1;
+
+	if (len > 0)
+		msk = msk << len;
+
+	while (msk > 0)
+	{
+		if (n & msk)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		msk >>= 1;
+	}
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Directory containing all the 0x14-bit_manipulation tasks
